1d0dc773939b93307501f7b1bd0668560bf8e888
Ulugbek Bahrom <ulugbek.bahromov2@gmail.com>
2022-02-01 21:28:49 +0500

    Added lombok to the project.

--- src/main/java/com/bakhromov/sms/entity/Student.java
+++ src/main/java/com/bakhromov/sms/entity/Student.java
@@ -1,7 +1,15 @@
 package com.bakhromov.sms.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "students")
 public class Student {
@@ -19,44 +27,10 @@ public class Student {
     @Column(name = "email")
     private String email;
 
-    public Student() { }
-
     public Student(String firstName, String lastName, String email) {
         super();
         this.firstName = firstName;
         this.lastName = lastName;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
     }
 }
